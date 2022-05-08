aac1ba982fd4df624f5e38429d075a5fc2c8a8f0
Huevos <rowland@moralet.com>
2022-05-08 20:24:34 +0200

    Make changes to bouquets reversible
    Adds a yellow key to reinstate services that were hidden.

--- usr/lib/enigma2/python/Plugins/Extensions/BouquetCleanup/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/BouquetCleanup/plugin.py
@@ -15,7 +15,6 @@ from Components.Sources.StaticText import StaticText
 
 from Plugins.Plugin import PluginDescriptor
 
-from Screens.MessageBox import MessageBox
 from Screens.Screen import Screen, ScreenSummary
 from Screens.Setup import Setup
 
@@ -31,7 +30,7 @@ class BouquetCleanupSetup(Setup):
 	def createSetup(self):
 		self.list = []
 		self.list.append(getConfigListEntry("From", config.plugins.BouquetCleanup.source, "Source location of the bouquets you want to clean up. '/etc/enigma2' is the default location. The location '/tmp/bouquets' is just provided for testing purposes."))
-		self.list.append(getConfigListEntry("To", config.plugins.BouquetCleanup.target, "Target location where you want the cleaned bouquets to be saved. '/etc/enigma2' is the default location. The location in '/tmp/bouquets/new' is just provided for testing purposes. If the source and target locations are the same the new cleaned bouquets will overwrite the originals."))
+		self.list.append(getConfigListEntry("To", config.plugins.BouquetCleanup.target, "Target location where you want the cleaned bouquets to be saved. '/etc/enigma2' is the default location. The location in '/tmp/bouquets/new' is just provided for testing purposes. If the source and target locations are the same the new 'cleaned' bouquets will overwrite the originals."))
 		currentItem = self["config"].getCurrent()
 		self["config"].list = self.list
 		self.moveToItem(currentItem)
@@ -105,7 +104,7 @@ class BouquetsWriter():
 		default_path = "/etc/enigma2" # "/tmp/bouquets" #
 		self.path = default_path if path is None else path
 
-	def writeBouquets(self, bouquetsDict):
+	def writeBouquets(self, bouquetsDict, showAll=False):
 		for bouquet_type in ["tv", "radio"]:
 			exists(self.path) or makedirs(self.path) # just here for testing to alternative paths
 			bouquetIndex = []
@@ -118,11 +117,13 @@ class BouquetsWriter():
 					bouquet_current.write('\n'.join(current_bouquet_list))
 					bouquet_current.close()
 					del current_bouquet_list
-					# Hide empty bouquets, but, special case, don't ever hide userbouquet.LastScanned.tv or userbouquet.favourites.tv even if they are empty.
-					if not row["hasActiveServices"] and not row["filename"] == "userbouquet.LastScanned.tv" and not row["filename"] == "userbouquet.favourites.tv":
-						row_split = row["row"].split(":")
-						row_split[1] = str(int(row_split[1]) | eServiceReference.isInvisible)
-						row["row"] = ":".join(row_split)
+					row_split = row["row"].split(":")
+					# Hide empty bouquets, but, special case, don't ever hide userbouquet.LastScanned.tv or userbouquet.favourites.tv or userbouquet.favourites.radio even if they are empty.
+					if not showAll and not row["hasActiveServices"] and not row["filename"] in ("userbouquet.LastScanned.tv", "userbouquet.favourites.tv", "userbouquet.favourites.radio"):
+						row_split[1] = str(int(row_split[1]) | eServiceReference.isInvisible) # flip to invisible if was visible
+					else:
+						row_split[1] = str(int(row_split[1]) & ~eServiceReference.isInvisible) # flip to visible if was invisible
+					row["row"] = ":".join(row_split)
 				bouquetIndex.append(row["row"])
 			if bouquetIndex:
 				index_current = open(self.path + "/bouquets.%s" % bouquet_type, "w")
@@ -136,24 +137,31 @@ class BouquetCleanup(Screen):
 		Screen.__init__(self, session)
 		self.title = _("BouquetCleanup")
 		self.skinName = ["BouquetCleanup", "Setup"]
-		self["key_green"] = StaticText(_("Clean bouquets"))
+		self.greenText = _("Clean bouquets")
+		self.yellowText = _("Reinstate hidden")
+		self["key_green"] = StaticText(self.greenText)
+		self["key_yellow"] = StaticText(self.yellowText)
 		self["key_red"] = StaticText(_("Exit"))
 		self["key_menu"] = StaticText(_("MENU"))
-		self["saveactions"] = ActionMap(["CancelSaveActions", "OkCancelActions"],
+		self["saveactions"] = ActionMap(["ColorActions"],
 		{
-			"save": self.keySave,
-			"ok": self.keySave,
+			"green": self.keySave,
+		}, -3)
+		self["reinstateactions"] = ActionMap(["ColorActions"],
+		{
+			"yellow": self.showAll,
 		}, -3)
 		self["cancelactions"] = ActionMap(["CancelSaveActions", "MenuActions"],
 		{
 			"cancel": self.keyCancel,
 			"menu": self.keyMenu,
 		}, -3)
-		self["config"] = Label(_("Press 'GREEN' to run a clean-up on your bouquets. Channels on satellites that are not configured will be removed. Empty bouquets will be hidden. It is advisable to make a backup beforehand in case you want to reverse the changes.\n\nPress 'MENU' to access this plugin's configuration setup."))
+		self["config"] = Label(_("Press 'GREEN' to run a clean-up on your bouquets.\nChannels on satellites that are not configured will be hidden.\nEmpty bouquets will also be hidden.\nRun again after any tuner reconfiguration.\n\nPress 'YELLOW' to reinstate all hidden services.\n\nPress 'MENU' to access this plugin's configuration setup."))
 
 		self.bouquetsDict = {}
 		self.active_orbitals = []
 		self.summaryCallbacks = []
+		self.hideNotConfiguredServices = True # opposite is reinstate all services
 
 		for nim in nimmanager.nim_slots:
 			if nim.isCompatible("DVB-S"):
@@ -168,18 +176,19 @@ class BouquetCleanup(Screen):
 		self.active_orbitals = sorted(list(dict.fromkeys(self.active_orbitals)))
 
 	def keySave(self):
-		message = _("Cleaning bouquets makes irreversible changes.\nMake a backup first in case you want to reverse the changes.\nAre you sure you want to proceed?")
-		self.session.openWithCallback(self.keySaveCallback, MessageBox, message, MessageBox.TYPE_YESNO, title=self.title)
-	
-	def keySaveCallback(self, answer):
-		if answer:
-			self.timer = eTimer()
-			self.timer.callback.append(self.processBouquets)
-			self.timer.start(100, 1)
-			self["config"].setText(_("Currently running a clean-up on your bouquets."))
-			self["saveactions"].setEnabled(False)
-			self["key_green"].text = ""
-			self.updateSummary()
+		self.timer = eTimer()
+		self.timer.callback.append(self.processBouquets)
+		self.timer.start(100, 1)
+		self["config"].setText(_("The process is currently running. Please wait!"))
+		self["saveactions"].setEnabled(False)
+		self["reinstateactions"].setEnabled(False)
+		self["key_green"].text = ""
+		self["key_yellow"].text = ""
+		self.updateSummary()
+
+	def showAll(self):
+		self.hideNotConfiguredServices = False
+		self.keySave()
 
 	def keyMenu(self):
 		self.session.open(BouquetCleanupSetup)
@@ -193,24 +202,37 @@ class BouquetCleanup(Screen):
 					for j, item in enumerate(row["content"][:]):
 						if item.startswith("#SERVICE ") and not ":http" in item:
 							item_split = item.split(":")
-							if len(item_split) > 7 and int(item_split[1]) == 0:
-								if(int(item_split[6], 16) >> 16) not in self.active_orbitals:
-									self.bouquetsDict[bouquet_type][i]["content"][j] = self.spacer(item)
+							if len(item_split) > 7 and (int(item_split[1]) & ~eServiceReference.isInvisible) == 0: # playable
+								if self.hideNotConfiguredServices and (int(item_split[6], 16) >> 16) not in self.active_orbitals:
+									self.bouquetsDict[bouquet_type][i]["content"][j] = self.invisible(item_split)  # flip to invisible if was visible
 								else:
+									self.bouquetsDict[bouquet_type][i]["content"][j] = self.reinstate(item_split) # flip to visible if was invisible
 									numActiveServices += 1
 						elif item.startswith("#SERVICE ") and ":http" in item:
 							numActiveServices += 1 # count IPTV as active service
 					self.bouquetsDict[bouquet_type][i]["hasActiveServices"] = numActiveServices
-		BouquetsWriter(config.plugins.BouquetCleanup.target.value).writeBouquets(self.bouquetsDict)
+		BouquetsWriter(config.plugins.BouquetCleanup.target.value).writeBouquets(self.bouquetsDict, showAll=not self.hideNotConfiguredServices)
 		# reload
 		if config.plugins.BouquetCleanup.target.value == config.plugins.BouquetCleanup.target.default:
 			eDVBDB.getInstance().reloadServicelist()
 			eDVBDB.getInstance().reloadBouquets()
-		self["config"].setText(_("Bouquet clean-up complete."))
+		self["config"].setText(_("Process complete."))
 		self.updateSummary()
-							
-	def spacer(self, item):
-		return "#SERVICE 1:320:0:0:0:0:0:0:0:0:%s\n#DESCRIPTION  %s" % ("\r" if "\r" in item else "", "\r" if "\r" in item else "")	
+		if self.hideNotConfiguredServices:
+			self["reinstateactions"].setEnabled(True)
+			self["key_yellow"].text = self.yellowText
+		else:
+			self["saveactions"].setEnabled(True)
+			self["key_green"].text = self.greenText
+		self.hideNotConfiguredServices = True # return to default state
+
+	def invisible(self, item_split):
+		item_split[1] = str(int(item_split[1]) | eServiceReference.isInvisible)
+		return ":".join(item_split)
+
+	def reinstate(self, item_split):
+		item_split[1] = str(int(item_split[1]) & ~eServiceReference.isInvisible)
+		return ":".join(item_split)
 
 	def keyCancel(self):
 		self.close()
